14f7c4cf1240c0c5adcc327da7cf1a59e9d39ea2
AgeDim <proga778@gmail.com>
2022-04-21 21:43:30 +0300

    Lab 5 second

--- Lab5/func.py
+++ Lab5/func.py
@@ -24,14 +24,14 @@ def gettask(task_id):
         if a == 0 or b == 0:
             print("К сожалению делить на ноль нельзя. Знаю печалька, но по другому никак)")
             sys.exit(1)
-        return lambda x, y: y + (1 + x) * (y ** 2), \
+        return lambda x, y: y + (1 + x) * pow(y, 2), \
                lambda x: -1 / x, \
                a, \
                b, \
                temp
     elif task_id == '2':
-        return lambda x, y: (x ** 2) - 2 * y, \
-               lambda x: 0.75 * exp(-2 * x) + 0.5 * (x ** 2) - 0.5 * x + 0.25, \
+        return lambda x, y: pow(x, 2) - 2 * y, \
+               lambda x: 0.75 * exp(-2 * x) + 0.5 * pow(x, 2) - 0.5 * x + 0.25, \
                a, \
                b, \
                temp
